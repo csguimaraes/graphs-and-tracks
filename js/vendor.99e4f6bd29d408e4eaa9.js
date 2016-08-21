7bb8f89ab9f28d9c79030503d5442c6e29b2aba9
Carlos Guimaraes <snolflake@gmail.com>
2016-08-21 08:59:47 -0300

    Fixing component declarations;

--- js/vendor.99e4f6bd29d408e4eaa9.js
+++ js/vendor.99e4f6bd29d408e4eaa9.js
@@ -2513,4 +2513,4 @@ providers:[e.MD_SLIDE_TOGGLE_VALUE_ACCESSOR],changeDetection:o.ChangeDetectionSt
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 "use strict";var i=this&&this.__extends||function(t,e){function n(){this.constructor=t}for(var r in e)e.hasOwnProperty(r)&&(t[r]=e[r]);t.prototype=null===e?Object.create(e):(n.prototype=e.prototype,new n)},o=n(r),s=function(t){function e(){t.apply(this,arguments)}return i(e,t),Object.defineProperty(e.prototype,"formDirective",{get:function(){return null},enumerable:!0,configurable:!0}),Object.defineProperty(e.prototype,"path",{get:function(){return null},enumerable:!0,configurable:!0}),e}(o.AbstractControlDirective);e.ControlContainer=s}]);
-//# sourceMappingURL=vendor.4a08d453c5114099031d.js.map
+//# sourceMappingURL=vendor.99e4f6bd29d408e4eaa9.js.map
